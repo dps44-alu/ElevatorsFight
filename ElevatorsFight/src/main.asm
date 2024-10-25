70310c2228697a3c107208bab534c215f5257865
dps44-alu <dps44@alu.ua.es>
2024-10-25 13:30:27 +0200

    Funciones estructuradas y comentadas

--- ElevatorsFight/src/main.asm
+++ ElevatorsFight/src/main.asm
@@ -4,59 +4,189 @@ SECTION "Header", ROM0[$100]
 
 	jp EntryPoint
 
-	ds $150 - @, 0 ; Crea ROM para el Header
+	ds $150 - @, 0  ; Crea ROM para el Header
+
+
+wait_vblank_start:
+	.loop
+		ld a, [rLY]		; rLY = $FF44 -> Indica la línea actual que está siendo dibujada
+		cp 144			; Principio de VBLANK
+		jr c, .loop
+    ret
+
+
+wait_vblank_end:
+	.loop
+		ld a, [rLY]
+		cp 144
+		jr nc, .loop
+	ret
+
+
+switch_screen_off:
+    call wait_vblank_start
+    xor a
+	ld [rLCDC], a				; rLCDC = $FF40 -> Controla la pantalla, el bit 7 indica si está encendia (1) o apagada (0)
+    ret
+
+
+switch_screen_on:
+	ld a, LCDCF_ON | LCDCF_BGON | LCDCF_OBJON	; OR de encender_pantalla, habilitar_fondo y habilitar_sprites
+	ld [rLCDC], a
+	ret
+
+
+; Copia bytes de un área a otra
+; DE: Origen
+; HL: Destino
+; BC: Tamaño
+mem_copy:
+	.loop
+		ld a, [de]
+		ld [hl+], a
+		inc de
+		dec bc
+		ld a, b
+		or a, c
+		jr nz, .loop
+	ret
+
+
+; Pone la OAM a 0's
+; A: 0
+; B: Contador
+; HL: Puntero a la dirección de la OAM
+clear_oam:
+	xor a
+	ld b, 160			; Número e bytes en la OAM que controla los sprites
+	ld hl, _OAMRAM		; _OAMRAM = $FE00 -> Puntero al inicio de la OAM (Almacena las propiedades de los sprites)
+	.loop
+		ld [hl+], a
+		dec b
+		jr nz, .loop
+	ret
+
+
+my_ret:
+	ret
+
+
+; Lee el estado de los botones y los guarda en A 
+; 1 nibble = 4 bits = 1/2 byte 
+; rP1 (P1/JOYP) -> Guarda el estado de los botones de la consola
+; 	-> Bits 0-3: indica el estado de los botones (A, B, Select, Start, Derecha, Izquierda, Arriba, Abajo)
+; 	-> Bits 4,5: indica en grupo de botones que se quiere leer, direcciones (Derecha, Izquierda, Arriba, Abajo) o botones (A, B, Select, Start)
+one_nibble:
+	ldh [rP1], a 	; rP1 = $FF00 -> Actualiza la matriz de teclas
+	call my_ret 	; Quema 10 ciclos llamando a un ret (Pausa)
+	ldh a, [rP1] 	; Ignorar para que la matriz de teclas se estabilice
+	ldh a, [rP1]
+	ldh a, [rP1] 	; Lee
+	or a, $F0 		; 11110000 -> 7-4 = 1, 3-0 = teclas no presionadas 
+	ret
+
+
+update_keys:
+	; Escribe la mitad del controllador (Botones A y B)
+	ld a, P1F_GET_BTN	; P1F_GET_BTN = P1F_4 = %00010000 -> Carga los botones
+	call one_nibble		
+	ld b, a 			; 11110000 -> 7-4 = 1, 3-0 = botones no presionados
+
+	; Escribe la otra mitad (Direcciones)
+	ld a, P1F_GET_DPAD	; P1F_GET_DPAD = P1F_5 = %00100000 -> Carga las direcciones
+	call one_nibble
+	swap a 				; A3-0 = direcciones no presionadas; A7-4 = 1
+	xor b 				; A = botones presionados + direcciones
+	ld b, a 			; B = botones presionados + direcciones
+
+	; Carga los controladores
+	ld a, P1F_GET_NONE	; P1F_GET_NONE = OR(P1F_4, P1F_5) = OR(%00010000, %00100000) -> No carga ninguno
+	ldh [rP1], a
+
+	; Combina con las wCurKeys previas para crear las wNewKeys
+	ld a, [wCurKeys]	; wCurKeys -> Teclas que estaban presionadas anteriormente (Estado actual de los botones)
+	xor b 				; A = teclas que han cambiado de estado
+	and b 				; A = teclas que han cambiado a presionadas
+	ld [wNewKeys], a	; wNewKeys -> Teclas recién presionadas (Estado nuevo de los botones)
+	ld a, b
+	ld [wCurKeys], a	; wCurKeys = estado actualizado de las teclas
+
+	ret
+
+
+move:
+	; Comprueba del botón izquierdo
+	.check_left
+		ld a, [wCurKeys]
+		and PADF_LEFT			; PADF_LEFT = $20 = %00100000 -> Bit correspondiente al botón izquierdo
+		jp z, .check_right		; Si el botón no está presionado, salta a check_right
+
+	.left
+		; Mueve la nave un pixel a la izquierda
+		ld a, [_OAMRAM + 1]  	; Se accede a la posición X de la nave, OAM -> 0:Y, 1:X, 2:INDICETILE, 3:ATRIBUTOS
+		dec a					; Se mueve 1 pixel a la izquierda
+		cp 15					; 15 = $0F = %00001111 -> Borde izquierda
+		jp z, main				; Si es 0, está en el borde y no se mueve
+		ld [_OAMRAM + 1], a		; Si no lo está, actualiza el valor de X de la nave
+		jp main
+
+	; Comprueba del botón derecho
+	.check_right
+		ld a, [wCurKeys]
+		and a, PADF_RIGHT		; PADF_RIGHT = $10 = %00010000 -> Bit corresponidente al botón derecho
+		jp z, main				; Si el botón no está presionado, salta a main
+
+	.right
+		; Mueve la nave un pixel a la derecha
+		ld a, [_OAMRAM + 1]		; Se accede a la posición X de la nave
+		inc a					; Se mueve 1 pixel a la derecha
+		cp 105					; 105 = $69 = %01101001 -> Borde derecha
+		jp z, main				; Si es 0, está en el borde y no se mueve
+		ld [_OAMRAM + 1], a		; Si no lo está, actualiza el valor de X de la nave
+		jp main
+
+
+main:
+	call wait_vblank_end
+	call wait_vblank_start
+	call update_keys
+	jp move 
+	ret
+
 
 EntryPoint:
-	; No apagar la pantalla fuera de VBLANK
-
-WaitVBlank:
-	ld a, [rLY]
-	cp 144				; Principio de VBLANK
-	jp c, WaitVBlank
-
-	; Apaga la pantalla
-	xor a
-	ld [rLCDC], a	
+    call switch_screen_off
 
 	; Copia los tiles
 	ld de, Tiles
 	ld hl, $9000
 	ld bc, TilesEnd - Tiles
-	call Memcopy
+	call mem_copy
 
 	; Copia el tilemap
 	ld de, Tilemap
 	ld hl, $9800
 	ld bc, TilemapEnd - Tilemap
-	call Memcopy
+	call mem_copy
 
-	; Copia el tile de la nave
+	; Copia los tiles de la nave
 	ld de, nave
 	ld hl, $8000
 	ld bc, naveend - nave
-	call Memcopy
+	call mem_copy
 
+	call clear_oam
+
+	ld hl, _OAMRAM
+	ld a, 128 + 16		
+	ld [hl+], a			; Posición en el eje Y -> 144
+	ld a, 16 + 8		
+	ld [hl+], a			; Posición en el eje X -> 24
 	xor a
-	ld b, 160			; Número de bytes en la OAM que controla los sprites
-	ld hl, _OAMRAM
+	ld [hl+], a			; Primer tile en la memoria de tiles
+	ld [hl], a			; Sprite sin propiedades especiales
 
-ClearOam:
-	ld [hl+], a
-	dec b
-	jr nz, ClearOam
-
-	ld hl, _OAMRAM
-	ld a, 128 + 16
-	ld [hl+], a
-	ld a, 16 + 8
-	ld [hl+], a
-	ld a, 0
-	ld [hl+], a
-	ld [hl], a
-
-	; Enciende la pantalla
-	ld a, LCDCF_ON | LCDCF_BGON | LCDCF_OBJON
-	ld [rLCDC], a
+	call switch_screen_on
 
 	; Durante el primer frame de VBLANk, inicializa los registros display
 	ld a, %11100100
@@ -70,101 +200,8 @@ ClearOam:
 	ld [wCurKeys], a
 	ld [wNewKeys], a
 
-Main:
-	ld a, [rLY]
-	cp 144
-	jp nc, Main
-	
-WaitVBlank2:
-	ld a, [rLY]
-	cp 144
-	jp c, WaitVBlank2
+	jp main
 
-	; Conprueba las teclas actuales cada frame y mueve a la izquierda o la derecha 
-	call UpdateKeys
-
-	; Primero comprueba si el botón de la izquierda ha sido presionado
-CheckLeft:
-	ld a, [wCurKeys]
-	and a, PADF_LEFT
-	jp z, CheckRight
-
-Left:
-	; Mueve la nave un pixel a la izquierda
-	ld a, [_OAMRAM + 1]  	; OAM -> 0:Y, 1:X, 2:INDICETILE, 3:ATRIBUTOS
-	dec a
-	; Si está en el borde, no se mueve
-	cp a, 15
-	jp z, Main
-	ld [_OAMRAM + 1], a
-	jp Main
-
-; Comprueba la tecla de la derecha
-CheckRight:
-	ld a, [wCurKeys]
-	and a, PADF_RIGHT
-	jp z, Main
-
-Right:
-	; Mueve la nave un pixel a la derecha
-	ld a, [_OAMRAM + 1]
-	inc a
-	; Si está en el borde, no se mueve
-	cp a, 105
-	jp z, Main
-	ld [_OAMRAM + 1], a
-	jp Main
-
-UpdateKeys:
-	; Escribe la mitad del controllador
-	ld a, P1F_GET_BTN
-	call .onenibble		; 1 nibble = 4 bits = 1/2 bytes
-	ld b, a 			; B7-4 = 1; B3-0 = botones no presionados
-
-	; Escribe la otra mitad
-	ld a, P1F_GET_DPAD
-	call .onenibble
-	swap a 				; A3-0 = direcciones no presionadas; A7-4 = 1
-	xor a, b 			; A = botones presionados + direcciones
-	ld b, a 			; B = botones presionados + direcciones
-
-	; Carga los controladores
-	ld a, P1F_GET_NONE
-	ldh [rP1], a
-
-	; Combina con las wCurKeys previas para crear las wNewKeys
-	ld a, [wCurKeys]
-	xor a, b 			; A = teclas que han cambiado de estado
-	and a, b 			; A = teclas que han cambiado a presionadas
-	ld [wNewKeys], a
-	ld a, b
-	ld [wCurKeys], a
-	ret
-
-	.onenibble
-		ldh [rP1], a 		; Cambia la matriz de teclas
-		call .knownret 		; Quema 10 ciclos llamando a un ret
-		ldh a, [rP1] 		; Ignorar para que la matriz de teclas se estabilice
-		ldh a, [rP1]
-		ldh a, [rP1] 		; Lee
-		or a, $F0 			; A7-4 = 1; A3-0 = teclas no presionadas
-	
-	.knownret
-		ret
-
-; Copia bytes de un área a otra
-; DE: Origen
-; HL: Destino
-; BC: Tamaño
-Memcopy:
-	ld a, [de]
-	ld [hl+], a
-	inc de
-	dec bc
-	ld a, b
-	or a, c
-	jp nz, Memcopy
-	ret
 
 Tiles:
 	dw `33333333
@@ -401,11 +438,11 @@ TilemapEnd:
 
 
 
-SECTION "Counter", WRAM0
+SECTION "Contador", WRAM0
 wFrameCounter: db
 
-; ANCHOR: vars
-SECTION "Input Variables", WRAM0
+
+
+SECTION "Variables de entrada", WRAM0
 wCurKeys: db
 wNewKeys: db
-; ANCHOR_END: vars
