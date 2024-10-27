66c8e5f4e619c0dc2cafc80652632c541259b0f3
mas117-ua <mas117@alu.ua.es>
2024-10-27 13:22:43 +0100

    balas sin bug pero solo 1 bala

--- ElevatorsFight/src/objects/bullets.asm
+++ ElevatorsFight/src/objects/bullets.asm
@@ -1,85 +1,136 @@
-include "src/hardware.inc"
-
+include "hardware.inc"
 SECTION "BulletVariables", WRAM0
-
-wBulletActive: ds 1         ; Indica si la bala está activa (1) o inactiva (0)
-wBulletPosX: ds 1           ; Posición X de la bala
-wBulletPosY: ds 1           ; Posición Y de la bala
+wBulletActive: ds 1        ; Indica si la bala está activa (1) o inactiva (0)
+wBulletPosX: ds 1          ; Posición X de la bala
+wBulletPosY: ds 1          ; Posición Y de la bala
+wBulletOAMIndex: ds 1      ; Índice fijo en OAM para la bala
+wLastShotTime: ds 1        ; Contador para el debounce del disparo
 
 SECTION "Bullet", ROM0
 
 initializeBullet:
-    ; Cargar el gráfico de la bala en VRAM en la dirección $8010
-    ld de, bala_vertical          ; Dirección de origen del gráfico de la bala
-    ld hl, $8010                  ; Dirección en VRAM donde se cargará el gráfico
+    ; Cargar el gráfico de la bala en VRAM
+    ld de, bala_vertical          
+    ld hl, $8010                  
     ld bc, bala_vertical_end - bala_vertical
-    call mem_copy                 ; Copiar los datos del gráfico en VRAM
+    call mem_copy                 
 
-    ; Inicializamos la bala como inactiva y fuera de pantalla
+    ; Inicializar variables
     xor a
-    ld [wBulletActive], a         ; Inactivo (0)
-    ld a, 0                       ; Posición inicial X (fuera de pantalla)
+    ld [wBulletActive], a        
     ld [wBulletPosX], a
-    ld [wBulletPosY], a           ; Posición inicial Y (fuera de pantalla)
+    ld [wBulletPosY], a   
+    ld [wLastShotTime], a       
+    
+    ; Establecer índice fijo en OAM
+    ld a, 4                      ; 4 bytes después de la nave
+    ld [wBulletOAMIndex], a
+    
+    ; Limpiar el sprite inicial
+    call ClearBulletSprite
+    ret
+
+ClearBulletSprite:
+    ; Mover el sprite de la bala fuera de la pantalla
+    ld a, [wBulletOAMIndex]
+    ld c, a
+    ld b, 0
+    ld hl, _OAMRAM
+    add hl, bc
+    
+    ; Y = 0 (fuera de pantalla)
+    xor a
+    ld [hl+], a
+    ; X = 0
+    ld [hl+], a
+    ; Tile = 0
+    ld [hl+], a
+    ; Atributos = 0
+    ld [hl], a
     ret
 
 FireBullet::
-    ; Activa la bala si no está activa y la coloca en la posición de la nave
+    ; Verificar si la bala ya está activa
     ld a, [wBulletActive]
     cp 1
-    ret z                         ; Si ya está activa, no hacer nada
+    ret z               
 
-    ; Activa la bala y establece su posición en la posición de la nave
+    ; Verificar el debounce timer
+    ld a, [wLastShotTime]
+    cp 0
+    ret nz
+
+    ; Activar la bala
     ld a, 1
     ld [wBulletActive], a
+    
+    ; Establecer el tiempo de debounce
+    ld a, 30           ; 15 frames de espera entre disparos
+    ld [wLastShotTime], a
+    
+    ; Posicionar la bala centrada sobre la nave
     ld a, [posicionNaveX]
     ld [wBulletPosX], a
+    
     ld a, [posicionNaveY]
+    sub 8               ; Colocar encima de la nave
     ld [wBulletPosY], a
     ret
 
 UpdateBullet::
-    ; Actualiza la posición de la bala solo si está activa
+    ; Actualizar el timer de debounce
+    ld a, [wLastShotTime]
+    cp 0
+    jr z, .skipTimerUpdate
+    dec a
+    ld [wLastShotTime], a
+.skipTimerUpdate:
+
+    ; Verificar si la bala está activa
     ld a, [wBulletActive]
     cp 1
-    ret nz                        ; Si no está activa, no hacer nada
+    ret nz              
 
-    ; Mueve la bala hacia arriba
+    ; Mover la bala hacia arriba
     ld a, [wBulletPosY]
-    sub 1                          ; Reducir Y para mover la bala hacia arriba
+    sub 2               ; Velocidad de la bala
     ld [wBulletPosY], a
-    cp 0
-    jp z, DeactivateBullet         ; Desactiva la bala si sale de la pantalla
-
-    ; Dibujar la bala en una posición específica de la OAM
-    ld hl, _OAMRAM + 4             ; Fija la posición en la OAM para la bala (4 bytes después de la nave)
-
-    ; Escribir la posición Y sin incrementar `hl`
+    
+    ; Verificar si salió de la pantalla
+    cp 16              ; 16 es el límite superior visible
+    jp c, DeactivateBullet
+    
+    ; Actualizar el sprite en OAM durante VBLANK
+    call wait_vblank_start
+    
+    ld a, [wBulletOAMIndex]
+    ld c, a
+    ld b, 0
+    ld hl, _OAMRAM
+    add hl, bc
+    
+    ; Actualizar Y
     ld a, [wBulletPosY]
-    ld [hl], a
-
-    ; Escribir la posición X
-    inc hl
+    ld [hl+], a
+    
+    ; Actualizar X
     ld a, [wBulletPosX]
-    ld [hl], a
-
-    ; Número de tile para la bala (01 en VRAM en $8010)
-    inc hl
+    ld [hl+], a
+    
+    ; Tile de la bala
     ld a, $01
-    ld [hl], a                     ; Tile de la bala
-
-    ; Atributos de la bala (sin flip, paleta 0)
-    inc hl
+    ld [hl+], a
+    
+    ; Atributos
     xor a
     ld [hl], a
     ret
 
-
-
 DeactivateBullet::
-    ; Desactiva la bala y la mueve fuera de pantalla
     xor a
-    ld [wBulletActive], a          ; Desactiva la bala
-    ld [wBulletPosX], a            ; Posición fuera de pantalla
-    ld [wBulletPosY], a            ; Posición fuera de pantalla
+    ld [wBulletActive], a
+    
+    ; Limpiar el sprite durante VBLANK
+    call wait_vblank_start
+    call ClearBulletSprite
     ret
